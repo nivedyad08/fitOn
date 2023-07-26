976ac960063549a20fe279d8ebedad17508df65c
Nivedya Divakaran <d.nivedya@gmail.com>
2023-07-26 16:10:35 -0700

    changed styles in landing page

--- client/src/Pages/Landing/PricingPlans.jsx
+++ client/src/Pages/Landing/PricingPlans.jsx
@@ -37,7 +37,7 @@ const PricingPlans = ({ userPlans, packages }) => {
                                 ))
                             }
                         </ul>
-                        <button className="mt-10 w-2/3 transform rounded-md bg-white text-custom-yellow px-4 py-2 capitalize tracking-wide transition-colors duration-300 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-200 focus:ring-opacity-80 font-semibold" onClick={ () => navigate("/login") }>Start Now</button>
+                        <button className="mt-10 w-2/3 transform rounded-md bg-[#ffebcd] text-custom-yellow px-4 py-2 capitalize tracking-wide transition-colors duration-300 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-200 focus:ring-opacity-80 font-semibold" onClick={ () => handlePayment("basic") }>Start Now</button>
                     </div>
                 ) }
                 <div className="w-full h-[355px] space-y-8 rounded-lg bg-[#353656] p-8 text-center transition-all hover:-translate-y-2">
@@ -74,7 +74,7 @@ const PricingPlans = ({ userPlans, packages }) => {
                             ))
                         }
                     </ul>
-                    <button className="mt-10 w-2/3 transform rounded-md bg-white text-custom-yellow px-4 py-2 capitalize tracking-wide transition-colors duration-300 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-200 focus:ring-opacity-80" onClick={ () => handlePayment("ultimate") }>Start Now</button>
+                    <button className="mt-10 w-2/3 transform rounded-md bg-[#ffebcd] text-custom-yellow px-4 py-2 capitalize tracking-wide transition-colors duration-300 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-200 focus:ring-opacity-80" onClick={ () => handlePayment("ultimate") }>Start Now</button>
                 </div>
             </div>
 
